14b6a7655d8fb0b72e082aedea3b9ca2a64aac81
Samuel Andrade de Araújo <andradedearaujo100@gmail.com>
2024-07-25 21:44:43 -0300

    Setting up database.

---
+++ src/database/database.ts
@@ -0,0 +1,12 @@
+import { Sequelize } from 'sequelize-typescript';
+
+const sequelize = new Sequelize({
+  database: process.env.POSTGRES_DB || "",
+  password: process.env.POSTGRES_PASSWORD || "",
+  username: process.env.POSTGRES_USER || "",
+  host: process.env.POSTGRES_HOST || "",
+  dialect: (process.env.POSTGRES_DIALECT as any) || "postgres",
+  models: [__dirname + "/models"]
+});
+
+export default sequelize;
